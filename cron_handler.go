dab6382a4d990d7c304714aa0a17ab5135179666
sinmetal <metal.tie@gmail.com>
2019-02-19 19:34:39 +0900

    add Change Spanner NodeNumber Logic
    Cloud Schedulerから貰った値で実際にSpannerのNodeの数を変更する処理を作成。
    JobをSpannerに投げているだけで、最終的な結果までは確認していない。

--- cron_handler.go
+++ cron_handler.go
@@ -3,7 +3,11 @@ package smagnet
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/spanner/v1"
 )
 
 // PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
@@ -15,7 +19,7 @@ type PubSubMessage struct {
 type SpannerMagnetMessage struct {
 	ProjectID  string `json:"projectId"`
 	InstanceID string `json:"instanceId"`
-	NodeNumber int    `json:"nodeNumber"`
+	NodeNumber int64  `json:"nodeNumber"`
 }
 
 // HandleSpannerMagnet consumes a Pub/Sub message.
@@ -26,6 +30,28 @@ func HandleSpannerMagnet(ctx context.Context, m PubSubMessage) error {
 		return err
 	}
 	log.Printf("SpannerMagnetMessage is %+v", msg)
+
+	client, err := google.DefaultClient(ctx, spanner.SpannerAdminScope)
+	if err != nil {
+		log.Printf("google.DefaultClient, %+v", err)
+		return err
+	}
+	admin, err := spanner.New(client)
+	if err != nil {
+		log.Printf("spanner.New, %+v", err)
+		return err
+	}
+	resp, err := admin.Projects.Instances.Patch(fmt.Sprintf("projects/%s/instances/%s", msg.ProjectID, msg.InstanceID), &spanner.UpdateInstanceRequest{
+		Instance: &spanner.Instance{
+			NodeCount: msg.NodeNumber,
+		},
+		FieldMask: "nodeCount",
+	}).Do()
+	if err != nil {
+		log.Printf("failed change spanner node count. %+v", err)
+		return err
+	}
+	fmt.Printf("Response : %+v", resp)
 
 	return nil
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gcpug/smagnet
+
+require cloud.google.com/go v0.36.0 // indirect
